055e573e034ca956a0471966aa248a4aa809204a
sorada <sprathan@my.centennialcollege.ca>
2023-10-29 15:21:45 -0400

    Innitial Commit with the project style and sidebar

---
+++ apps/ngshop/src/app/pages/product-list/product-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'collectibaba-product-list',
+  templateUrl: './product-list.component.html'
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
